af0ea2f4fd089fda155c81f3f5a9075f88f03a82
samksksk <98775007+eusamk@users.noreply.github.com>
2024-01-28 11:47:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function sendNotification() {
+    const image = document.getElementById('imageInput').value;
+    const title = document.getElementById('titleInput').value;
+    const message = document.getElementById('messageInput').value;
+
+    if (Notification.permission !== 'granted') {
+        Notification.requestPermission().then(permission => {
+            if (permission === 'granted') {
+                showNotification(image, title, message);
+            }
+        });
+    } else {
+        showNotification(image, title, message);
+    }
+}
+
+function showNotification(image, title, message) {
+    const notification = new Notification(title, {
+        body: message,
+        icon: image
+    });
+}
