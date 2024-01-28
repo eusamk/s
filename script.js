b0d735827ecc99e28e2e25578196810568342ebc
samksksk <98775007+eusamk@users.noreply.github.com>
2024-01-28 18:36:15 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,57 @@
-function validateCard() {
-  const cardNumberInput = document.getElementById('cardNumber');
-  const resultElement = document.getElementById('result');
+let saldo = 0;
 
-  const cardNumber = cardNumberInput.value.replace(/\s/g, ''); // Remover espaços em branco
-
-  if (isValidCard(cardNumber)) {
-    resultElement.textContent = 'Cartão válido!';
-    resultElement.style.color = 'green';
-  } else {
-    resultElement.textContent = 'Cartão inválido. Por favor, verifique o número.';
-    resultElement.style.color = 'red';
-  }
+function depositar() {
+    const valor = parseFloat(prompt("Digite o valor a ser depositado:"));
+    if (!isNaN(valor) && valor > 0) {
+        saldo += valor;
+        atualizarSaldo();
+    } else {
+        alert("Digite um valor válido para depósito.");
+    }
 }
 
-function isValidCard(cardNumber) {
-  const digits = cardNumber.split('').map(Number);
-  const reversedDigits = digits.reverse();
-
-  for (let i = 1; i < reversedDigits.length; i += 2) {
-    reversedDigits[i] *= 2;
-    if (reversedDigits[i] > 9) {
-      reversedDigits[i] -= 9;
+function sacar() {
+    const valor = parseFloat(prompt("Digite o valor a ser sacado:"));
+    if (!isNaN(valor) && valor > 0 && valor <= saldo) {
+        saldo -= valor;
+        atualizarSaldo();
+    } else {
+        alert("Digite um valor válido para saque.");
     }
-  }
+}
 
-  const sum = reversedDigits.reduce((acc, digit) => acc + digit, 0);
-  return sum % 10 === 0;
+function atualizarSaldo() {
+    document.getElementById("saldo").innerText = `Saldo: $${saldo.toFixed(2)}`;
+}
+
+function reproduzirSom() {
+    const somAposta = document.getElementById("somAposta");
+    somAposta.currentTime = 0;  // Reinicia o áudio para garantir que possa ser reproduzido várias vezes
+    somAposta.play();
+}
+
+function apostar(escolha) {
+    const valorApostado = parseFloat(document.getElementById("valorOperacao").value);
+    if (isNaN(valorApostado) || valorApostado <= 0 || valorApostado > saldo) {
+        alert("Digite um valor válido para a aposta.");
+        return;
+    }
+
+    reproduzirSom();  // Chama a função para reproduzir o som
+
+    const moeda = Math.random() < 0.5 ? "cara" : "coroa";
+    document.getElementById("face").setAttribute("src", `${moeda}.png`);
+    document.getElementById("face").classList.add("animate__animated", "animate__flipInY");
+
+    setTimeout(() => {
+        document.getElementById("face").classList.remove("animate__flipInY");
+        const resultado = moeda === escolha ? "Ganhou!" : "Perdeu!";
+        if (moeda === escolha) {
+            saldo += valorApostado * 1.5;
+        } else {
+            saldo -= valorApostado;
+        }
+        atualizarSaldo();
+        document.getElementById("result").innerText = `Resultado: ${resultado}`;
+    }, 1000);
 }
